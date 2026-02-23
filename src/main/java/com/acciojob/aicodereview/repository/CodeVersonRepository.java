50de01c65c352cdc302d660920613dc0b8c4fe78
FrostScythe <maheshwariayush371@gmail.com>
2026-02-23 12:09:29 +0530

    Backend commit

---
+++ src/main/java/com/acciojob/aicodereview/repository/CodeVersonRepository.java
@@ -0,0 +1,15 @@
+package com.acciojob.aicodereview.repository;
+
+import com.acciojob.aicodereview.model.CodeVersion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CodeVersonRepository extends JpaRepository<CodeVersion, UUID> {
+
+    List<CodeVersion> findBySubmissionId(UUID submissionId);
+
+    Optional<CodeVersion> findTopBySubmissionIdOrderByVersionNumberDesc(UUID submissionId);
+}
